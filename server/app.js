925b328b6736fb2db1a4befcb38440f33e3a0bd5
Desire <desire_guilarte@hotmail.com>
2020-10-06 01:48:35 -0400

    Last build that's good before trying to mess with crypto

--- server/app.js
+++ server/app.js
@@ -1,17 +1,20 @@
 const express = require('express')
 const app = express()
+const {PORT = 3000} = process.env
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 require('./User')
+require('dotenv').config();
 
 app.use(bodyParser.json())
 
 const User = mongoose.model("user")
 
 
-mongoose.connect(mongoUri,{
+mongoose.connect(process.env.MONGO_URI,{
     useNewUrlParser:true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
 })
 
 mongoose.connection.on("connected",() =>{
@@ -21,20 +24,55 @@ mongoose.connection.on("error",(err) =>{
     console.log("error", err)
 })
 app.get('/', (req,res) => {
-    res.send("welcome to node js")
+    User.find({}).then(data=>{
+        res.send(data)
+    }).catch(err=>{
+        console.log(err)
+    })
 })
 
-app.post('/senddata', (req, res)=>{
+app.post('/send-data', (req, res)=>{
     const user = new User({
         user_id:req.body.user_id,
         email:req.body.email,
         family_name:req.body.family_name,
-        given_name:req.body.given_name,
+        given_name:req.body.given_name
 
     })
-    res.send("posted")
+    user.save()
+    .then(data=>{
+        console.log(data)
+        res.send("posted")
+    }).catch(err=>{
+        console.log(err)
+    })
+    
 })
 
-app.listen(3000, () => {
+app.post('/delete',(req,res)=>{
+    User.findByIdAndRemove(req.body.id)
+    .then(data=>{
+        console.log(data)
+        res.send("deleted")
+    }).catch(err=>{
+        console.log(err)
+    })
+})
+
+app.post('/update', (req, res)=>{
+    User.findByIdAndUpdate(req.body.id,{
+        user_id:req.body.user_id,
+        email:req.body.email,
+        family_name:req.body.family_name,
+        given_name:req.body.given_name
+    })
+    .then(data=>{
+        console.log(data)
+    }).catch(err=>{
+        console.log(err)
+    })
+})
+
+app.listen(PORT, () => {
     console.log("server running")
 })
